0e258d2f931ab530ecb0686cdccc2bffca3ad881
alext <aleksey.tertyshnyk@winningtemp.com>
2026-08-30 21:45:53 +0300

    memory efficient version

--- 01_sensor_stats.cpp
+++ 01_sensor_stats.cpp
@@ -2,34 +2,28 @@
 #include <vector>
 
 int main() {
-    int N=0;
-    std::vector<double> readings;
-
-    std::cin >> N;
-    readings.resize(N);
-
     double max=0;
     double min=0;
     double summ=0;
+    int N = 0;
 
-    for (int i=0;i<N;i++) {
-        double reading;
-        std::cin >> reading;
-        readings[i] = reading;
-
+    double reading;
+    while (std::cin >> reading) {
         if (reading < -40 || reading > 85) {
             std::cout << "reading outside the range!\n";
         }
 
         summ = summ + reading;
-        if (i==0) {
-            max = readings[0];
-            min = readings[0];
+        if (N==0) {
+            max = reading;
+            min = reading;
         } else if (reading > max) {
             max = reading;
         } else if (reading < min) {
             min = reading;
         }
+
+        N++;
     }
 
     double avg = summ/N;
